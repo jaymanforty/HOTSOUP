25630435f4aadbdd825f6ff47e9c465012687943
Jordan <jordanfonger@gmail.com>
2022-08-28 04:52:53 -0600

    added filter to keep trolls at bay

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -1,4 +1,3 @@
-from imp import new_module
 import disnake
 import os
 import openai
@@ -43,9 +42,16 @@ class ChatbotCog(commands.Cog):
             model="text-davinci-002",
             prompt=prompt,
             max_tokens = 50,
-            temperature = 0.9)
+            temperature = 0.9
+            )
 
             response_text = r['choices'][0]['text']
+
+            output_label = self.get_filter_label(response_text)
+            if output_label == 2: 
+                print("Prompt was too toxic")
+                return
+
             estimated_cost = r['usage']['total_tokens'] * (6/1000)
             print("cost ~ ¢", estimated_cost)
             await msg.reply(response_text)
@@ -79,6 +85,12 @@ class ChatbotCog(commands.Cog):
         )
 
         response_text = r['choices'][0]['text']
+
+        output_label = self.get_filter_label(response_text)
+        if output_label == 2:
+            await ctx.send(embed=disnake.Embed(description="Too toxic!"))
+            return
+
         estimated_cost = r['usage']['total_tokens'] * (6/1000)
         embed_str = f"""
         {ctx.author.mention} - {message}
@@ -89,7 +101,55 @@ class ChatbotCog(commands.Cog):
         await ctx.send(embed=disnake.Embed(description=embed_str).set_footer(text=f"¢{estimated_cost:.2f}"))
 
 
+    def get_filter_label(self, content):
 
+        response = openai.Completion.create(
+        model="content-filter-alpha",
+        prompt="<|endoftext|>"+content+"\n--\nLabel:",
+        temperature=0,
+        max_tokens=1,
+        top_p=0,
+        logprobs=10)
+        
+        output_label = response["choices"][0]["text"]
+
+        # This is the probability at which we evaluate that a "2" is likely real
+        # vs. should be discarded as a false positive
+        toxic_threshold = -0.355
+
+        if output_label == "2":
+            # If the model returns "2", return its confidence in 2 or other output-labels
+            logprobs = response["choices"][0]["logprobs"]["top_logprobs"][0]
+
+            # If the model is not sufficiently confident in "2",
+            # choose the most probable of "0" or "1"
+            # Guaranteed to have a confidence for 2 since this was the selected token.
+            if logprobs["2"] < toxic_threshold:
+                logprob_0 = logprobs.get("0", None)
+                logprob_1 = logprobs.get("1", None)
+
+                # If both "0" and "1" have probabilities, set the output label
+                # to whichever is most probable
+                if logprob_0 is not None and logprob_1 is not None:
+                    if logprob_0 >= logprob_1:
+                        output_label = "0"
+                    else:
+                        output_label = "1"
+                # If only one of them is found, set output label to that one
+                elif logprob_0 is not None:
+                    output_label = "0"
+                elif logprob_1 is not None:
+                    output_label = "1"
+
+                # If neither "0" or "1" are available, stick with "2"
+                # by leaving output_label unchanged.
+
+        # if the most probable token is none of "0", "1", or "2"
+        # this should be set as unsafe
+        if output_label not in ["0", "1", "2"]:
+            output_label = "2"
+
+        return output_label
 
 
 def setup(bot):
