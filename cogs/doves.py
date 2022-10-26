7e4f1bedd29df230e9fd2099a9dc142758b48a9c
Jordan <jordanfonger@gmail.com>
2022-10-25 21:08:31 -0600

    removed command

--- cogs/doves.py
+++ cogs/doves.py
@@ -7,61 +7,6 @@ from disnake.ext import commands
 class DovesCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot: commands.InteractionBot = bot
-        self.mute_doves_users = {}
-        self.mute_doves_timestamp = None
-
-    @commands.Cog.listener('on_message')
-    async def doves_message(
-        self,
-        message: disnake.Message) -> None:
-
-        if message.author.id == self.bot.user.id: return
-        if not self.mute_doves_timestamp: return
-
-        try:
-            if self.mute_doves_users[message.author.id] <= time.time():
-                self.mute_doves_users.pop(message.author.id)
-            else:
-                await message.delete()
-        except: KeyError
-
-        
-    @commands.slash_command()
-    async def only_doves_can_use_this(
-        self,
-        ctx: disnake.ApplicationCommandInteraction,
-        user_id: str) -> None:
-        """
-        Mute whoever has muted you
-        """
-        g = self.bot.get_guild(int(798935679366594571))
-        user = g.get_member(int(user_id))
-        if ctx.author.id != 452655064989958164:
-            await ctx.send("Nice try", ephemeral=True)
-            return
-        if user.id == 452655064989958164 or user.id not in self.mute_doves_users:
-            await ctx.send("Nice try bird",ephemeral=True)
-            return
-        s = rnd.randint(150,300)
-        await ctx.send(f"Muted {user.display_name} for {s} seconds")
-        self.mute_doves_users[user.id] = time.time() + s
-
-    @commands.slash_command()
-    @commands.cooldown(1,86400, commands.BucketType.member)
-    async def mute_doves(
-        self,
-        ctx: disnake.ApplicationCommandInteraction) -> None:
-        """
-        Mute doves for being cringe, but careful that means he can mute you too!
-        """
-        if ctx.author.id == 452655064989958164:
-            await ctx.send("Nice try bird", ephemeral=True)
-            return
-
-        s = rnd.randint(30,300)
-        await ctx.send(f"Muted doves for {s} seconds")
-        self.mute_doves_users[ctx.author.id] = time.time()
-        self.mute_doves_users[452655064989958164] = time.time() + s
 
     @commands.slash_command()
     @commands.cooldown(1,86400, commands.BucketType.member)
