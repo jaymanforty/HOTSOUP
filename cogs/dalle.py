6c4c08a25d937c1e0f9ddc49d91fff1fa804d232
Jordan <jordanfonger@gmail.com>
2022-11-18 01:12:15 -0700

    dalle image gen

---
+++ cogs/dalle.py
@@ -0,0 +1,30 @@
+import disnake
+import os
+import openai
+from disnake.ext import commands
+
+from models.database import Database
+
+class DalleCog(commands.Cog):
+    def __init__(self,bot) -> None:
+        self.bot: commands.InteractionBot = bot
+        self.db = Database()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+    @commands.slash_command()
+    @commands.cooldown(1,86400, commands.BucketType.member)
+    async def create_image(
+        self,
+        ctx: disnake.ApplicationCommandInteraction,
+        prompt: str) -> None:
+        """
+        AI generated image
+        """
+        await ctx.response.defer()
+        response = openai.Image.create(prompt=prompt,n=1,size="256x256")
+        image_url = response['data'][0]['url']
+        await ctx.send(image_url)
+
+def setup(bot):
+    bot.add_cog(DalleCog(bot))
