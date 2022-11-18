2bdb1763beca8553ce9a5cf4a686aae449594382
Jordan <jordanfonger@gmail.com>
2022-11-18 09:35:23 -0700

    store pics with filenames in case one or more people using command at a time

--- cogs/dalle.py
+++ cogs/dalle.py
@@ -3,6 +3,8 @@ import os
 import openai
 import shutil
 import requests
+import unicodedata
+import re
 from disnake.ext import commands
 
 from models.database import Database
@@ -32,10 +34,30 @@ class DalleCog(commands.Cog):
         response = openai.Image.create(prompt=prompt,n=1,size="1024x1024")
         image_url = response['data'][0]['url']
         res = requests.get(image_url,stream=True)
-        with open("temp_dalle.png",'wb') as f:
+        if not os.path.exists("dalle_pics"):
+            os.makedirs("dalle_pics")
+
+        with open(f"dalle_pics/{self.slugify(prompt)}.png",'wb') as f:
             shutil.copyfileobj(res.raw,f)
+
         img = disnake.File("temp_dalle.png")
         await ctx.send(file=img)
+
+    def slugify(self, value, allow_unicode=False):
+        """
+        Taken from https://github.com/django/django/blob/master/django/utils/text.py
+        Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+        dashes to single dashes. Remove characters that aren't alphanumerics,
+        underscores, or hyphens. Convert to lowercase. Also strip leading and
+        trailing whitespace, dashes, and underscores.
+        """
+        value = str(value)
+        if allow_unicode:
+            value = unicodedata.normalize('NFKC', value)
+        else:
+            value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+        value = re.sub(r'[^\w\s-]', '', value.lower())
+        return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 def setup(bot):
     bot.add_cog(DalleCog(bot))
